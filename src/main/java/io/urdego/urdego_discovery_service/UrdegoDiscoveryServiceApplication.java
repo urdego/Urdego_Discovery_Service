d8c0ec1ee68a85c02ac866ceead90c277d439246
ooni <gdbsrjsdn@gmail.com>
2024-12-27 16:03:10 +0900

    start project

---
+++ src/main/java/io/urdego/urdego_discovery_service/UrdegoDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package io.urdego.urdego_discovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrdegoDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrdegoDiscoveryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/urdego/urdego_discovery_service/UrdegoDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.urdego.urdego_discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrdegoDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
