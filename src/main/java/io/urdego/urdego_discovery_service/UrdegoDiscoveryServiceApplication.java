f8b8418ca56fbad3b60cbfe9fd3c821f76b18ff1
Ban-gilhyeon <bbgiloo98@gmail.com>
2025-01-11 00:02:21 +0900

    Feature : Eureka Server 의존성 추가 및 application 수정

--- src/main/java/io/urdego/urdego_discovery_service/UrdegoDiscoveryServiceApplication.java
+++ src/main/java/io/urdego/urdego_discovery_service/UrdegoDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package io.urdego.urdego_discovery_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class UrdegoDiscoveryServiceApplication {
 
     public static void main(String[] args) {
