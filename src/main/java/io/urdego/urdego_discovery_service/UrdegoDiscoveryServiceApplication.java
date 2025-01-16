e57e3d2af2d103d15f02b8fcea051c1b265d8348
JoungWoo Lee <j_ra1n@naver.com>
2025-01-16 22:53:40 +0900

    Spring Boot 버전 다운

--- src/main/java/io/urdego/urdego_discovery_service/UrdegoDiscoveryServiceApplication.java
+++ src/main/java/io/urdego/urdego_discovery_service/UrdegoDiscoveryServiceApplication.java
@@ -11,6 +11,5 @@ public class UrdegoDiscoveryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UrdegoDiscoveryServiceApplication.class, args);
     }
-
     
 }
