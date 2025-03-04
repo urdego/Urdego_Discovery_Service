41c7841008172de6d330cc5083257c8e6322ce5a
JoungWoo Lee <j_ra1n@naver.com>
2025-03-04 15:33:55 +0900

    Feature: 모니터링 의존성 추가

--- src/main/java/io/urdego/urdego_discovery_service/UrdegoDiscoveryServiceApplication.java
+++ src/main/java/io/urdego/urdego_discovery_service/UrdegoDiscoveryServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class UrdegoDiscoveryServiceApplication {
 
+    
     public static void main(String[] args) {
         SpringApplication.run(UrdegoDiscoveryServiceApplication.class, args);
     }
