a9d584cb20d543d1a2bd621cb69630ea33995d7b
JoungWoo Lee <j_ra1n@naver.com>
2025-04-07 17:13:56 +0900

    Refactor: Dockerfile 수정

--- src/main/java/io/urdego/urdego_discovery_service/UrdegoDiscoveryServiceApplication.java
+++ src/main/java/io/urdego/urdego_discovery_service/UrdegoDiscoveryServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class UrdegoDiscoveryServiceApplication {
+
     
     public static void main(String[] args) {
         SpringApplication.run(UrdegoDiscoveryServiceApplication.class, args);
